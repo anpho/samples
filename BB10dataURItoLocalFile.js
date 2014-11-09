2e43ea6ba7b768491266e9e496280fe8f72c26ab
Merrick Zhang <anphorea@users.noreply.github.com>
2014-11-09 13:18:15 +0800

    Create BB10dataURItoLocalFile.js

---
+++ BB10dataURItoLocalFile.js
@@ -0,0 +1,41 @@
+function dataURItoFile(dataURI, callback) {
+  // Convert dataURI to binary file content.
+	var byteString = atob(dataURI.split(',')[1]);
+	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+	var ab = new ArrayBuffer(byteString.length);
+	var ia = new Uint8Array(ab);
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i);
+	}
+	var dataview = new DataView(ab);
+	var bb = new Blob([dataview], {
+		type: mimeString
+	});
+	// bb is file content.
+
+
+	window.webkitRequestFileSystem(window.PERSISTENT, 5 * 1024 * 1024, function(fs) {
+		console.log('Opened file system: ' + fs.name);
+		fs.root.getFile(blackberry.io.home+"/"+new Date().getTime() + '.png', {
+			create: true,
+			exclusive: true
+		}, function(fileEntry) {
+			var fe = fileEntry;
+			fe.createWriter(function(writer) {
+				var fw = writer;
+				fw.onwriteend = function() {
+					console.log('write end')
+					var fp=fe.toURL();
+					callback("file://"+fp.substr(fp.indexOf('/accounts/'));
+				}
+				fw.write(bb);
+			}, function(e) {
+				callback(null);
+			})
+		}, function(e) {
+			callback(null);
+		});
+	}, function(e) {
+		callback(null);
+	});
+}
