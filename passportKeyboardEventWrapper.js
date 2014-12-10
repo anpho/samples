b5bf7579bc4e25e361c8f88e0ec1b12ec9c680f6
Merrick Zhang <anphorea@users.noreply.github.com>
2014-12-10 09:17:45 +0800

    Update passportKeyboardEventWrapper.js

--- passportKeyboardEventWrapper.js
+++ passportKeyboardEventWrapper.js
@@ -1,5 +1,6 @@
 /*
  * Sample Binding Code, NOTE that this event should be binded after "deviceready" was fired.
+ * 注意：此代码需在webworks中以以下方式绑定到事件，且只绑定一次。
  * document.addEventListener("deviceready", function () {
  *   document.addEventListener("touchenabledkeyboard", touchKeyboardCallback);
  *  });
@@ -7,13 +8,18 @@
  */
 
 var touchHandler=function(event){
-	//event is swipedown  / swipeup  / swipeleft  / swiperight 
-	//document.getElementById('app').innerHTML = event
+  //event is swipedown  / swipeup  / swipeleft  / swiperight 
+  //document.getElementById('app').innerHTML = event
   //handle events here.
+  //在此函数内处理不同的事件。
 }
 
+// 事件存储区
+// Event Storage
 var touchenabledkeyboardEvents={};
 
+// The least distance you move your finger on the keyboard to trigger this function.
+// 触发事件的最小移动距离
 var threshold=20;
 
 function touchKeyboardCallback(event) {
@@ -21,6 +27,7 @@ function touchKeyboardCallback(event) {
         function (touch){
             console.log(JSON.stringify(touch));
             if (touch.state=="TouchReleased"){
+            	//取出这个事件的起点
 	            var originalPoint=touchenabledkeyboardEvents[touch.identifier];
 	            if (originalPoint){
 		            var dx=touch.keyboardX - originalPoint.x;
@@ -38,9 +45,11 @@ function touchKeyboardCallback(event) {
 				            touchHandler("swipeup")
 			            }
 		            }
+		            //删除事件起点
 		            originalPoint=touchenabledkeyboardEvents[touch.identifier]=null;
 	            }
             }else if (touch.state=="TouchPressed"){
+            	//如果手指碰到键盘，记下当时的位置（X/Y）和时间，这个点是事件的起点
 	            touchenabledkeyboardEvents[touch.identifier] = {
 		            x:touch.keyboardX,
 		            y:touch.keyboardY,
